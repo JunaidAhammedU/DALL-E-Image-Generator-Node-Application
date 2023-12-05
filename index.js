09032b7dc324a6b8cc201146aef35e1cae9fee60
JunaidAhammedU <junaidahammed15@gmail.com>
2023-12-05 12:14:59 +0530

    Bugs fixed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser");
 const chatGPTRoutes = require("./Routes/chatGPT");
+require("dotenv").config();
 
 app.use("/chatGPT", chatGPTRoutes);
 app.use(express.json());
+
 app.use((err, req, res, next) => {
   console.error(err.message);
   res.status(500).send("an error occured");
 });
 //---------------------------------------
 
-app.listen(3000, () => {
-  console.log("server started");
+app.listen(process.env.PORT, () => {
+  console.log(`server running on port ${process.env.PORT}`);
 });
